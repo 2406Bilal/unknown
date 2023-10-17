8285b84a0917b813a331df0398639766b79f3567
2406Bilal <bilolismonov6@gmail.com>
2023-10-17 03:52:39 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="home">
+    <h1>home</h1>
+    <input type="text" v-model="search">
+    <p> search term - {{ search}}</p>
+    <p v-for="name in matchingNames" :key="name"> {{ name }}</p>
+  </div>
+</template>
+
+<script>
+import { computed, ref } from 'vue';
+export default {
+  name: 'Home',
+  setup() {
+    const search = ref('')
+    const names = ref([' bilol', ' hasan', ' huseyn', 'awur', ' botasli'])
+
+    const matchingNames = computed(() => {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+
+    return { names, search , matchingNames}
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+      <h1>home</h1>
+      <p>my name is {{ name }} and my age is {{ age }} </p>
+      <button @click="updateOne">click me to add </button>
+  
+    </div>
+  </template>
+  <script>
+  export default {
+    name: 'Home',
+    setup() {
+      let name = 'bilol'
+      let age = 20
+      return { name: name, age: age }
+    }
+  }
+  </script>
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
+  
+  nav {
+    padding: 30px;
+  }
+  
+  nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+  
+  nav a.router-link-exact-active {
+    color: #42b983;
+  }
+  </style>
+  
